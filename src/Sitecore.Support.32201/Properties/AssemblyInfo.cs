4d268b8ef0e9c8abc9347c9f401c900aad7792da
Kirill Volovitskiy <kiv@sitecore.net>
2017-10-18 11:29:58 +0300

    default PublishCandidateSource

---
+++ src/Sitecore.Support.32201/Properties/AssemblyInfo.cs
@@ -0,0 +1,6 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Sitecore.Support.32201")]
+[assembly: AssemblyProduct("Sitecore.Support.32201")]
+[assembly: ComVisible(false)]
